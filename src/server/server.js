d0b577108b675a387725a14521e461f8dc35c78a
pablomartan <pablomartan@kolabnow.com>
2023-01-13 21:49:20 +0100

    feat: built functionality to get latitude and longitud from user input

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(express.static('dist'));
 
 // API keys
-const GEO_API = process.env.GEONAMES_API_KEY;
+const GEO_API = `&username=${process.env.GEONAMES_API_KEY}`;
 const WEA_API = process.env.WEATHER_API_KEY;
 const PIX_API = process.env.PIXABAY_API_KEY;
 
@@ -20,10 +20,23 @@ const GEO_URL = 'http://api.geonames.org/searchJSON?q=';
 //const WEA_URL
 //const PIX_URL
 
-app.post('/city', (req, res) => {
+const getLatLng = async (city) => {
+    await fetch(GEO_URL+city+GEO_API)
+    .then(async response => { 
+        const parsed = await response.json();
+        const latLng = { 'lat': parsed.geonames[0].lat, 'lng': parsed.geonames[0].lng };
+        console.log(latLng);
+        return latLng
+        response.send(JSON.stringify(latLng));
+    })
+    .catch( error => { console.log(error) })
+}
+
+app.post('/city', async (req, res) => {
     const city = req.body.city;
     console.log(city);
-    res.send('OK');
+    const latLng = await getLatLng(city);
+    res.send(latLng);
 });
 
 app.listen(port, () => {
