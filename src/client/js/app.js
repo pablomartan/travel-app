19a96ad3bae360fd51b1070b8fce21ea87d2f1fa
pablomartan <pablomartan@kolabnow.com>
2023-01-14 12:56:08 +0100

    feat: prepared function to get weather based on coordinates and date

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -28,27 +28,37 @@ const queryGeoNames = async (cityName) => {
  *
 */
 
-const getWeather = async (cityName) => {
-    await queryGeoNames(cityName)
+const getWeather = async (city, date) => {
+    await queryGeoNames(city)
     .then(async geoRes => {
         const latLng = await geoRes.json();
         console.log(latLng);
         return latLng;
     })
+    .then(async latLng => {
+        const coordPluDate = { latLng, date };
+        console.log(coordPluDate);
+        return coordPluDate
+    });
 }
 
 /*
  * Retrieving the submit button, defining the function to handle the user
  * input, and adding the event listener
 */
-const form = document.getElementById('form-container');
+const formCollection = document.getElementsByTagName('form');
+const form = formCollection[0];
 
 const onSubmitLocation = (element) => {
     element.preventDefault();
 
-    const locationInput = document.getElementById('location-input');
-    const city = { 'city': locationInput.value };
-    getWeather(city);
+    const city = { 'city': form[0].value };
+    const date = {
+        'start': form[1].value,
+        'end': form[2].value
+    }
+
+    getWeather(city, date);
 }
 
 form.addEventListener('submit', onSubmitLocation);
