4d2d3a3cb4c30f56c72a67401e40d82aeaa9d86d
pablomartan <pablomartan@kolabnow.com>
2023-01-14 18:16:19 +0100

    change: split some functionality into various files

---
+++ src/client/js/updateUi.js
@@ -0,0 +1,14 @@
+/*
+ *
+ * HELPER FUNCTIONS
+ *
+*/
+
+/**
+ * @description: Creates the element for holding the date
+ * @param {Object} date: an object with the start and end dates of the trip
+*/
+const dateContainer = (date) => {
+    const container = document.createElement('div');
+    container.classList.add('date-container');
+};
