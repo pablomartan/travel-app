eea697243521f92924283f9e0154a4e95395d7bc
pablomartan <pablomartan@kolabnow.com>
2023-01-14 16:40:18 +0100

    feat: client now calls server for API keys and retrieves information from Geonames and Pixabay for the given city

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,3 +1,15 @@
+let geoKey;
+const geoUrl = 'http://api.geonames.org/searchJSON?username=';
+let weaKey;
+const weaUrl = 'https://api.weatherbit.io/v2.0/forecast/daily'; 
+let pixKey;
+const pixUrl = 'https://pixabay.com/api/?key=';
+
+const keyPairs = {
+    'geo': geoKey,
+    'wea': weaKey,
+    'pix': pixKey
+};
 
 /**
  *
@@ -6,21 +18,78 @@
 */
 
 /**
- * @description: queries the GeoNames API with the location name provided by
- * the user and returns the latitude and longitud of said location
- * @param {String} cityName: the name of the location (presumed to be a city)
+ * @description: queries the GeoNames API with a location name and extracts the
+ * latitude and longitude information from the first entry (the one with most
+ * resemblance to the given name)
+ * @param {String} city: the location name
 */
-const queryGeoNames = async (cityName) => {
-    const query = await fetch('http://localhost:8081/city', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(cityName)
-        })
-    return query;
+const getLatLng = async (baseUrl, apiKey, city) => {
+    console.log(`Querying for coordinates of city ${city}`);
+    const coords = await fetch(baseUrl + apiKey + `&q=${city}`)
+    .then(async geoRes => { 
+        const parsed = await geoRes.json();
+        const latLng = { 'lat': parsed.geonames[0].lat, 'lng': parsed.geonames[0].lng };
+        //console.log(latLng);
+        return latLng;
+    })
+    .catch( error => { console.log(error) })
+
+    return coords;
 }
+
+/**
+ * @description: queries the Pixabay API with a location name, gets the first
+ * image related to that search and sends it back to the client
+ * @param {String} city: the location name
+*/
+const getPic = async (baseUrl, apiKey, city) => {
+    console.log(`Querying for picture of city ${city}`);
+    const pic = await fetch(baseUrl + apiKey + `&q=${city}&type=photo`)
+    .then( async response => {
+        const parsed = await response.json();
+        const url = parsed.hits[0].largeImageURL;
+        return url;
+    })
+    return pic;
+}
+
+/**
+ * @description: retrieves an API key dictionary from server
+ * @param {String}: the URL to fetch
+*/
+const retrieveKeys = async (url) => {
+    await fetch(url)
+    .then(async response => {
+        const keys = await response.json();
+        geoKey = keys.geo;
+        weaKey = keys.wea;
+        pixKey = keys.pix;
+    })
+    .catch(error => {
+        console.log(`Error while retrieving keys. See message: ${error}`);
+    })
+};
+
+/**
+ * @description: collects the data from the different APIs and sends it to the
+ * function for updating the UI 
+ * @param {String} city: the city name
+ * @param {Object} date: the starting and ending dates the trip is planned for
+*/
+const collectData = async (city, date) => {
+    retrieveKeys('http://localhost:8081/keyPairs')
+    .then(async () => {
+        const latLng = await getLatLng(geoUrl, geoKey, city);
+        //const weather = await getWeater(latLng, date, weaUrl, weaKey);
+        const pic = await getPic(pixUrl, pixKey, city);
+        const data = {
+            'city': city,
+            //'weather': weather,
+            'pic': pic
+        };
+        return data;
+    })
+};
 
 /**
  *
@@ -28,19 +97,6 @@ const queryGeoNames = async (cityName) => {
  *
 */
 
-const getWeather = async (city, date) => {
-    await queryGeoNames(city)
-    .then(async geoRes => {
-        const latLng = await geoRes.json();
-        console.log(latLng);
-        return latLng;
-    })
-    .then(async latLng => {
-        const coordPluDate = { latLng, date };
-        console.log(coordPluDate);
-        return coordPluDate
-    });
-}
 
 /*
  * Retrieving the submit button, defining the function to handle the user
@@ -52,13 +108,13 @@ const form = formCollection[0];
 const onSubmitLocation = (element) => {
     element.preventDefault();
 
-    const city = { 'city': form[0].value };
+    const city = form[0].value;
     const date = {
         'start': form[1].value,
         'end': form[2].value
     }
 
-    getWeather(city, date);
+    collectData(city, date);
 }
 
 form.addEventListener('submit', onSubmitLocation);
