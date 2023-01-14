54844c991f67e0b3dead50be3f44e8b424f008e6
pablomartan <pablomartan@kolabnow.com>
2023-01-14 16:36:37 +0100

    feat: complete restructuring of the project; the rubric forces all the functionality into the client

--- src/server/server.js
+++ src/server/server.js
@@ -11,39 +11,19 @@ app.use(bodyParser.json());
 app.use(express.static('dist'));
 
 // API keys
-const GEO_API = `&username=${process.env.GEONAMES_API_KEY}`;
-const GEO_LIMIT = '&maxrows=1';
+const GEO_API = process.env.GEONAMES_API_KEY;
 const WEA_API = process.env.WEATHER_API_KEY;
 const PIX_API = process.env.PIXABAY_API_KEY;
 
-// API base urls
-const GEO_URL = 'http://api.geonames.org/searchJSON?q=';
-//const WEA_URL =
-const PIX_URL = `https://pixabay.com/api/?key=${PIX_API}`;
+const apiKeys = {
+    'geo': GEO_API,
+    'wea': WEA_API,
+    'pix': PIX_API
+};
 
-
-/**
- * @description: queries the GeoNames API with a location name and extracts the
- * latitude and longitude information from the first entry (the one with most
- * resemblance to the given name)
- * @param {Request} req: the request object from the fetch
- * @param {Response} res: the response object from the fetch
-*/
-const getLatLng = async (req, res) => {
-    const CITY = req.body.city;
-
-    await fetch(GEO_URL+CITY+GEO_LIMIT+GEO_API)
-    .then(async geoRes => { 
-        const parsed = await geoRes.json();
-        const latLng = { 'lat': parsed.geonames[0].lat, 'lng': parsed.geonames[0].lng };
-        console.log(latLng);
-        res.send(JSON.stringify(latLng));
-        return latLng;
-    })
-    .catch( error => { console.log(error) })
-}
-
-app.post('/city', getLatLng);
+app.get('/keyPairs', (req, res) => {
+    res.send(JSON.stringify(apiKeys));
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
