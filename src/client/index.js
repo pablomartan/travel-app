a372c948adf3033ae4a5fa4e6443893e494dcd79
pablomartan <pablomartan@kolabnow.com>
2023-01-17 10:46:31 +0100

    feat: added styles and removed unused ones

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,24 @@
 import { onSubmitLocation } from './js/app'
-import { getLatLng, getPic } from './js/queryApis'
+import {
+    getLatLng,
+    getPic,
+    getWeather 
+} from './js/queryApis'
+
 import { updateUi } from './js/updateUi'
 
 import './styles/_variables.scss'
-import './styles/style.scss'
+import './styles/_colorpalette.scss'
+import './styles/base.scss'
+import './styles/header.scss'
+import './styles/form.scss'
+import './styles/tripCard.scss'
+import './styles/footer.scss'
 
 export {
     onSubmitLocation,
     getLatLng,
+    getWeather,
     getPic,
     updateUi
 }
