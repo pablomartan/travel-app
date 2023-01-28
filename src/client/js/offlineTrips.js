a89cbc7cceadf3a227b0c17d806de76f3ec760bf
pablomartan <pablomartan@kolabnow.com>
2023-01-28 18:05:03 +0100

    refactor: removed unnecesary functions

--- src/client/js/offlineTrips.js
+++ src/client/js/offlineTrips.js
@@ -2,39 +2,6 @@
  * HELPER FUNCTIONS
 */
 
-/**
- * @description: gets the background url of a given HTML Element
- * @param {HTMLElement} el: the HTML element
-*/
-const getBgUrl = (el) => {
-    return url;
-};
-
-/**
- * @description: downloads the city pic to a file and sets the background to it
- * @param {HTMLElement} trip: the trip card
-*/
-const offlinePic = (trip) => {
-    let pic;
-    for (let i = 0; i < trip.childNodes.length; i++) {
-        const el = trip.childNodes[i];
-        if (el.classList.contains('trip-pic')) {
-            pic = el;
-        }
-    };
-    const bg = pic.style.background;
-    const url = bg.slice(4, -1).replace(/"/g, "");
-    const a = document.createElement('a');
-    a.href = getBgUrl(pic);
-    a.download = `${trip.id}.png`;
-    a.style.display = 'none';
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-
-    return pic;
-};
-
 /*
  * MAIN FUNCTIONALITY
 */
@@ -44,13 +11,12 @@ const offlinePic = (trip) => {
 const displayOfflineTrips = () => {
     if (localStorage.getItem('trips') != undefined) {
         const tripArray = JSON.parse(localStorage.getItem('trips'));
-        console.log(tripArray);
         const tripContainer = document.getElementById('trip-container') || Client.insertPlanContainer(Client.createPlanContainer());
         tripArray.forEach((trip) => {
             const div = document.createElement('div');
             div.innerHTML = trip;
             const extractedTrip = div.firstChild;
-            tripContainer.appendChild(offlinePic(extractedTrip));
+            tripContainer.appendChild(extractedTrip);
         });
     }
 };
